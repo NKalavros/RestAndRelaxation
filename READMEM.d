5e89d3037f64e7aab594c3ef429da8bdd5190113
Nikolaos Kalavros <39969236+NKalavros@users.noreply.github.com>
2024-10-30 11:42:41 -0400

    Create READMEM.d

---
+++ READMEM.d
@@ -0,0 +1,29 @@
+# Why is version control important?
+
+## Transparency and tracability:
+
+Version control allows us to "move back in analysis time", as in, trace previous versions of scripts, parameters, executions, etc. Alongside good documentation, this can help better describe the scientific process. Additionally, in case of breaking changes, they can always be reverted back. Allows scrutiny from other researchers if need be, leading to less PubPeer issues.
+
+## Collaboration:
+
+It's far easier to make sure that if a collaborator runs the "same code", the results remain the same if there is some kind of template, documentation for parameters and software versions.
+
+## Environments:
+
+Very similar to the previous point, this can assist other researchers who might want to build upon a computational method.
+
+## Free and open sharing:
+
+GitHub is free to use, though subject to Microsoft's whims and can be private while a project is in progress and public whenever a researcher wishes for academics.
+
+## Community Engagement:
+
+Wikis, issues, pull requests allow the computational research community to band together, building far more robust and performant software, as well as quickly weeding out issues, instead of relying on a sole maintainer. The downside of this is that sometimes there is a sole chief maintainer, which can lead to issues like the [https://access.redhat.com/security/cve/cve-2024-3094](.xz library CVE).
+
+## GitHub Actions:
+
+Automated frameworks for testing and validation of results allow painless updates to software
+
+### Link to Colab homework:
+
+[https://colab.research.google.com/drive/15O_cttHuP3V0UV6QDLDDxMlVKKKwvvkW?usp=sharing](Homework)
